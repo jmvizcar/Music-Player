db8cbc33c5f8ed06d8bd4675c82f7d78a4a677a7
Jesus M. Vizcarra <jmvizcarra95@gmail.com>
2022-09-03 03:32:24 -0700

    Add Constructor to MusicPlayer class

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MediaPlayer;
+class MusicPlayer
+{
+  // Music Player variable
+  WMPLib.WindowsMediaPlayer MPlayer;
+
+  public MusicPlayer()
+  {
+    MPlayer = new WMPLib.WindowsMediaPlayer();
+  }
+
+  private void PlayFile(string url)
+  {
+
+  }
+  
+}
+
