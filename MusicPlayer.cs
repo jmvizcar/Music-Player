83346a96ec579167239660d623524d5d277bcc82
Jesus M. Vizcarra <jmvizcarra95@gmail.com>
2022-09-09 20:28:56 -0700

    Add Play and Stop functionality to winform

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -8,8 +8,56 @@ public partial class MusicPlayer : Form
 
     public MusicPlayer()
     {
+        outputDevice = null!;
+        audioFile = null!;
         InitializeComponent();
     }
 
-    
+    private void InitializeComponent()
+    {
+        var flowPanel = new FlowLayoutPanel();
+        flowPanel.FlowDirection = FlowDirection.LeftToRight;
+        flowPanel.Margin = new Padding(10);
+
+        var buttonPlay = new Button();
+        buttonPlay.Text = "Play";
+        buttonPlay.Click += OnButtonPlayClick;
+        flowPanel.Controls.Add(buttonPlay);
+
+        var buttonStop = new Button();
+        buttonStop.Text = "Stop";
+        buttonStop.Click += OnButtonStopClick;
+        flowPanel.Controls.Add(buttonStop);
+
+        this.Controls.Add(flowPanel);
+    }
+
+    private void OnButtonPlayClick(object? sender, EventArgs args)
+    {
+        if (outputDevice == null)
+        {
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+        }
+        if (audioFile == null)
+        {
+            audioFile = new AudioFileReader(@"C:\Users\Jesus\Music\FFXIV\ENDWALKER 7-inch Vinyl Single\2_Endwalker - Footfalls.mp3");
+            outputDevice.Init(audioFile);
+
+        }
+        outputDevice.Play();
+    }
+    private void OnButtonStopClick(object? sender, EventArgs args)
+    {
+        outputDevice?.Stop();
+    }
+
+    private void OnPlaybackStopped(object? sender, StoppedEventArgs args)
+    {
+        outputDevice.Dispose();
+        outputDevice = null!;
+        audioFile.Dispose();
+        audioFile = null!;
+    }
+
 }
